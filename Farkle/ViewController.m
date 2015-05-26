6589319f5d7d1e58caf43049ff67268981a55793
jdivittorio3 <jdivittorio3@gmail.com>
2015-05-26 08:23:23 -0500

    credit to john for images

--- Farkle/ViewController.m
+++ Farkle/ViewController.m
@@ -36,6 +36,7 @@
         label.layer.cornerRadius = label.bounds.size.width / 10;
         label.layer.masksToBounds = YES;
         label.dieSelected = NO;
+        label.userInteractionEnabled = YES;
         [label rollDice];
     }
     self.rollButton.layer.cornerRadius = self.rollButton.bounds.size.width / 10;
@@ -141,5 +142,55 @@
         }
     }
 }
+
+// /////////////////////////////////// Gestures //////////////////////////////////////////////////
+
+//- (void)viewAppears {
+//    [super viewDidLoad];
+//    self.menuButtonOne = [self createButtonWithTitle:@"1"];
+//    [self.mainButton addTarget:self action:@selector(fanButtons:) forControlEvents:UIControlEventTouchUpInside];
+//    self.dynamicAnimator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
+//    self.areButtonsFanned = NO;
+//}
+//
+//-(UIButton *)createButtonWithTitle:(NSString *)title {
+//    CGRect frame = self.view.frame;
+//    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(frame.size.width - 50, frame.size.height - 50, 50, 50)];
+//    button.layer.cornerRadius = button.bounds.size.width / 2;
+//    button.layer.borderWidth = 1.0;
+//    button.backgroundColor = [UIColor blueColor];
+//    button.layer.borderColor = [[UIColor blackColor]CGColor];
+//    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [button setTitle:title forState:UIControlStateNormal];
+//
+//    [self.view addSubview:button];
+//    return button;
+//}
+//
+//-(void)fanButtons:(id)sender {
+//    [self.dynamicAnimator removeAllBehaviors];
+//    if (self.areButtonsFanned) {
+//        [self fanIn];
+//    } else {
+//        [self fanOut];
+//    }
+//    self.areButtonsFanned = !self.areButtonsFanned;
+//}
+//
+//-(void)fanOut {
+//    CGPoint point;
+//    UISnapBehavior *snapBehavior;
+//
+//    point = CGPointMake(self.mainButton.frame.origin.x - 50, self.mainButton.frame.origin.y + 20);
+//    snapBehavior = [[UISnapBehavior alloc]initWithItem:self.menuButtonOne snapToPoint:point];
+//    [self.dynamicAnimator addBehavior:snapBehavior];
+//}
+//
+//-(void)fanIn {
+//    UISnapBehavior *snapBehavior;
+//
+//    snapBehavior = [[UISnapBehavior alloc]initWithItem:self.menuButtonOne snapToPoint:self.mainButton.center];
+//    [self.dynamicAnimator addBehavior:snapBehavior];
+//}
 
 @end
